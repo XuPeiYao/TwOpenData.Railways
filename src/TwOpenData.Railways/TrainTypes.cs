986d09f2b8014044849f574447d25ddd5d7bf97c
徐培堯 <st89382000@gmail.com>
2016-08-03 01:27:27 +0800

    基本類別框架實作

---
+++ src/TwOpenData.Railways/TrainTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwOpenData.Railways {
+    /// <summary>
+    /// 車種
+    /// </summary>
+    public enum TrainTypes {
+        自強,
+        太魯閣,
+        普悠瑪,
+        莒光,
+        復興,
+        區間,
+        區間快車,
+        普快車,
+        觀光列車
+    }
+}
