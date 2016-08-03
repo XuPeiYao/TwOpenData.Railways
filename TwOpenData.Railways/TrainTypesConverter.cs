900c95c0d3f136d3afae7f88286492b1838643ce
徐培堯 <st89382000@gmail.com>
2016-08-03 16:23:04 +0800

    列車類型轉換器

---
+++ TwOpenData.Railways/TrainTypesConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwOpenData.Railways {
+    /// <summary>
+    /// 將列車類型轉換為主要列車類型(如普悠瑪為自強號)
+    /// </summary>
+    public class TrainTypesConverter {
+        /// <summary>
+        /// 將列車類型轉換為主要列車類型(如普悠瑪為自強號)
+        /// </summary>
+        /// <param name="type">列車類型</param>
+        /// <returns>主要列車類型</returns>
+        public TrainTypes Convert(TrainTypes type) {
+            return (TrainTypes)((int)Math.Round(((int)type)/ 10.0) * 10);
+        }
+    }
+}
